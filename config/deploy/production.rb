2410695d8673cf7adb4c2adc3c4ad08c1c7dd986
Gaston Sobrevilla <gsobrevilla@MacBook-Pro-de-Gaston-2.local>
2019-05-08 23:44:18 -0300

    change server name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :branch, 'master'
 set :deploy_to, '/home/deploy/webapp'
 set :rvm_ruby_version, '2.5.1@webapp'
 
-server "http://linode-2.vortexsoftware.com.ar", user: "deploy", roles: %w{app db web}
+server "50.116.42.99", user: "deploy", roles: %w{app db web}
